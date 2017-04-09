cc7278e0e17de7edfc74f110ad1624cbf4105061
Edwin Flores <eflores003@gmail.com>
2017-04-09 04:09:31 +0000

    28: Required users to enroll in a course to see its lessons

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,8 +1,22 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_authorized_for_current_course, only: [:show]
+
   def show
   end
 
   private
+
+  def require_authorized_for_current_course
+    if ! current_user.enrolled_in?(current_course)
+      redirect_to course_path(current_course), alert: 'You must enroll in the course to see its lessons.'
+    end
+  end
+
+  helper_method :current_course
+  def current_course
+    current_lesson.section.course
+  end
 
   helper_method :current_lesson
   def current_lesson
